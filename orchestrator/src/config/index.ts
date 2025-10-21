389d90a9abc3474a776994f6830b689be093e4d7
Divine Quansah <97671400+divquan@users.noreply.github.com>
2025-10-21 04:31:33 +0000

    remove sensitive data

--- orchestrator/src/config/index.ts
+++ orchestrator/src/config/index.ts
@@ -29,7 +29,7 @@ export const CONFIG = {
   AUDIT_LOGGER_URL: process.env.AUDIT_LOGGER_URL || 'http://localhost:3004',
   
   // Paystack Configuration
-  PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY || 'sk_test_45a6b86cd8f76273dbb4f46421f274acee89171f',
+  PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY,
   PAYSTACK_PUBLIC_KEY: process.env.PAYSTACK_PUBLIC_KEY || 'pk_test_81aec086991df0463d9f46f6df3bcbf2daa5ab3b',
   
   // Logging
